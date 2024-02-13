2013184e7a84d4037ba33c66c273cbfcfc923255
masiljangajji <hjk172262@gmail.com>
2024-02-13 16:23:26 +0900

    asd

---
+++ src/main/java/com/example/demo/asd.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+/**
+ * packageName    : com.example.demo
+ * fileName       : asd
+ * author         : iseungjae
+ * date           : 2/13/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/13/24        iseungjae       최초 생성
+ */
+public class asd {
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+    /**
+     * methodName : abcddd
+     * author : Jihun Park
+     * description :
+     *
+     * @param str
+     * @return string
+     */
+    public String abcddd(String str){
+        return "asd";
+    }
+
+}
---
+++ src/main/java/com/example/demo/controller/asd.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+/**
+ * packageName    : com.example.demo.controller
+ * fileName       : asd
+ * author         : iseungjae
+ * date           : 2/13/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/13/24        iseungjae       최초 생성
+ */
+public class asd {
+}
